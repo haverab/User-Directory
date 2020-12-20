dff1a1d067360bfc60f806dec263041fcc1627a0
Allison Haver <allisonbhaver@gmail.com>
2020-12-19 20:59:12 -0500

    updated api.js file

--- user/src/Utils/API.js
+++ user/src/Utils/API.js
@@ -1,9 +1,9 @@
-import  Axios from"axios";
+import  Axios from "axios";
 
 export default {
     // This function will use Axios to request 20 random users using https://randomuser.me/api/
     // Location of API: https://randomuser.me/
-    search: () => {
-        return Axios.get("https://randomuser.me/api/?results=20&nat=us");
+    getUsers:function(){    
+        return axios.get("https://randomuser.me/api/?results=50&nat=us&inc=name,email,phone,picture,login,id");
     },
 };
